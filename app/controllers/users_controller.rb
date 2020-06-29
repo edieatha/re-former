42211e815cb626823626153df0716d7fdc8ea04a
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-06-30 02:56:21 +0300

    1. Modify controller and routes to allow editing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+        @user.update(user_params)
+        redirect_to users_path
+    else
+        render :edit
+    end
+end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
