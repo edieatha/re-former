4a222a20d95f11a8f095b4f984d97c53c61a4aed
Edie Atha <edieatha@gmail.com>
2020-06-30 19:35:58 +0300

    Stickler errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def new
     @user = User.new
   end
@@ -17,18 +16,19 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.save
-        @user.update(user_params)
-        redirect_to edit_user_path
+      @user.update(user_params)
+      redirect_to edit_user_path
     else
-        render :edit
+      render :edit
     end
   end
   
   private
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
