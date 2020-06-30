65e49fd5ecaf0375d6f364cf8a4a70de026f0861
Edie Atha <edieatha@gmail.com>
2020-06-30 19:51:02 +0300

    Stickler errors fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_06_29_162041) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
