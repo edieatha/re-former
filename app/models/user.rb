63d81ef32da53450dd578c07fbb7d4020c37f873
Edie Atha <edieatha@gmail.com>
2020-06-30 19:56:35 +0300

    More stickler errors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true
   validates :password, presence: true, confirmation: true, on: :create
 end
